b2313045ea3fe017b3d5dffe33700bb347795f34
suibingbing <suibingbing@piesat.cn>
2024-06-26 17:12:24 +0800

    提交cmake工程

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/zlib_VS2022
+# Build directory: E:/zlib_VS2022/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "E:/zlib_VS2022/CMakeLists.txt;203;add_test;E:/zlib_VS2022/CMakeLists.txt;0;")
